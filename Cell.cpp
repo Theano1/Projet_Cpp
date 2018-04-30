b75fd914476ccb63bd6378345b2b1dc5500912a0
System Administrator <root@MacBook-nolwenn.local>
2018-04-30 18:03:04 +0200

    création de Cell.cpp et Cell.h

---
+++ Cell.cpp
@@ -0,0 +1,10 @@
+//This is the implementation file Cell.cpp of the class Cell.
+
+
+#include <iostream>
+using namespace std;
+#include "Cell.h"
+
+Cell::Cell(){
+	
+}
---
+++ Cell.h
@@ -0,0 +1,18 @@
+//This is the header file Cell.h.
+
+#include <iostream>
+using namespace std;
+
+class Cell {
+
+protected:
+	float _Aint;
+	float _Bint;
+	float _Cint;
+	float _Pmut;
+	float _Wmin;
+	float _Pdeath;
+	float _Raa, _Rab, _Rbb, _Rbc
+
+
+}
