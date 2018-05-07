def03bc44f006aa4bddb399dfec2df55d23af86b
Marie Codet <marie.codet@insa-lyon.fr>
2018-05-07 14:21:22 +0200

    Définition des getters

--- Environnement.cpp
+++ Environnement.cpp
@@ -15,7 +15,61 @@ Environnement::Environnement(int W, int H, float D, int T, float Ainit, float Tf
   Tfinal_=Tfinal;  
 }
 
+//=====================================================================================================
+//                                                    DESTRUCTOR
+//=====================================================================================================
+
+Environnement::~Environnement()=default;
 
 //=====================================================================================================
 //                                                    GETTERS
 //=====================================================================================================
+
+int Environnement::nb_S_init() {
+  return nb_S_init_;
+}
+
+int Environnement::nb_L_init() {
+  return nb_L_init_;
+}
+
+int Environnement::W() {
+  return W_;
+}
+
+int Environnement::H() {
+  return H_;
+}
+
+int Environnement::T() {
+  return T_;
+}
+
+float Environnement::Ainit() {
+return Ainit_;
+}
+
+float Environnement::Tfinal() {
+return Tfinal_;
+}
+  
+
+//=====================================================================================================
+//                                                    METHODS
+//==================================================================================================== 
+  
+void Environnement::diffusion(){
+
+}
+
+
+void Environnement::competition(){
+
+}
+   
+   
+   
+   
+   
+   
+   
