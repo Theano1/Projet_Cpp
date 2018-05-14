bd2145b09fa7886d77ff1bdbbf1c0e0260a0e05f
Nolwenn Guillaume <nolwenn.guillaume@insa-lyon.fr>
2018-05-14 14:32:56 +0200

    CellS implementation

--- CellS.cpp
+++ CellS.cpp
@@ -2,7 +2,110 @@
 //  CellS.cpp
 //  
 //
-//  
+//
 //
 
 #include "CellS.h"
+#include <iostream>
+using std::cout;
+using std::endl;
+
+//==============================
+//  STATIC ATTRIBUTES DEFINITION
+//==============================
+int CellS::compteur_S = 0; //On initialise notre compteur à 0
+
+//==============================
+//    CONSTRUCTORS
+//==============================
+
+CellS::CellS(){
+    
+    compteur_S++;
+    
+    type='S';
+    
+    Rbb=0.1;
+    Rbc=0.1;
+    
+}
+CellS::CellS(float a, float b, float c){
+    
+    compteur_S++;
+    
+    type='S';
+    
+    A_int=a;
+    B_int=b;
+    C_int=c;
+    
+    Rbb=0.1;
+    Rbc=0.1;
+    
+}
+
+//==============================
+//    DESTRUCTOR
+//==============================
+
+CellS::~CellS(){
+    compteur_S--;
+}
+
+//==============================
+//    PUBLIC METHODS
+//==============================
+
+Cell* CellS::Division(){
+    
+    A_int = A_int/2.0; //les individus fils héritent de la moitié des concentrations internes du parent
+    B_int = B_int/2.0;
+    C_int = C_int/2.0;
+    
+    CellS* newcell = new CellS(A_int, B_int, C_int); //nouvelle bactérie de type B qui récupère les nouveaux attributs
+    return newcell;
+    
+}
+
+void CellS::Describe(){
+    cout<< "Cette bactérie de type " <<type<<" présente une concentration interne en A: "<<A_int<<", en B: "<<B_int<<" et en C: "<<C_int<<endl<<" ainsi qu'une fitness de "<<w<<endl;
+    
+}
+
+int CellS::Death(){ return Cell::Death();}
+
+char CellS::Gettype(){return Cell::Gettype();}
+
+int CellS::Mute(){return Cell::Mute();}
+
+float CellS::GetA_int(){return A_int;}
+
+float CellS::GetB_int(){return B_int;}
+
+float CellS::GetC_int(){return C_int;}
+
+float& CellS::Getw(){return w;}
+
+int CellS::nombre_S()
+{
+    return compteur_S;   //On renvoie simplement la valeur du compteur
+}
+
+float CellS::absorb(float c, float h){ //système qui régit le réseau métabolique des individus de type Gb
+    
+    float newb=c;
+    float newbint=B_int;
+    float newcint=C_int;
+    
+    newb+=h*(-Rbb*c);
+    newbint+=h*(Rbb*c-Rbc*B_int);
+    newcint+=h*(Rbc*B_int);
+    
+    B_int=newbint;
+    C_int=newcint;
+    
+    return newb;
+    
+}
+
+
--- CellS.h
+++ CellS.h
@@ -1,11 +1,77 @@
 //
 //  CellS.h
-//  
+//
 //
 
 #ifndef CellS_h
 #define CellS_h
 
-#include <stdio.h>
+//==============================
+//    INCLUDES
+//==============================
+
+#include "Cell.h"
+
+class CellS: public Cell
+{
+public:
+    //==============================
+    //    CONSTRUCTORS
+    //==============================
+    CellS();
+    CellS(float a, float b, float c);
+    
+    
+    //==============================
+    //    DESTRUCTOR
+    //==============================
+    virtual ~CellS();
+    
+    //==============================
+    //    GETTERS
+    //==============================
+    virtual float GetA_int();
+    virtual float GetB_int();
+    virtual float GetC_int();
+    virtual char Gettype();
+    virtual float& Getw();
+    
+    
+    //==============================
+    //    SETTERS
+    //==============================
+    
+    //==============================
+    //    OPERATORS
+    //==============================
+    
+    //==============================
+    //    PUBLIC METHODS
+    //==============================
+    
+    virtual void Describe();
+    virtual Cell* Division();
+    virtual int Death();
+    virtual int Mute();
+    static int nombre_S();
+    virtual float absorb(float c, float h);
+    
+    
+protected:
+    //==============================
+    //    PROTECTED METHODS
+    //==============================
+    
+    //==============================
+    //    ATTRIBUTES
+    //==============================
+    float Rbb; //taux d'absorption
+    float Rbc; //taux de conversion
+    float& w=C_int; //fitness
+    static int compteur_S; //nombre de bactérie CellS
+    
+
+
+
 
 #endif /* CellS_h */
